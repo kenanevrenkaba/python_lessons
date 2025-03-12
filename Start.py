34c2f33e397a203ae8ef58cc7dd41c0b0b5684e3
KENAN EVREN KABA <knnevrn@gmail.com>
2025-03-12 23:38:28 +0300

    String Format ve f-String ile ilgili örnek kodlar yazdım.

--- Start.py
+++ Start.py
@@ -1,1 +1,18 @@
-print('Merhaba')
+# print('Merhaba')
+
+# String Format
+
+urunAdi = uA = 'Elma'
+urunFiyati = uF = 5
+urunMiktarı = uM = 2.5
+odenecekUcret = oU = urunFiyati * urunMiktarı 
+
+# sonuc = str(urunMiktarı) + ' kg '+ urunAdi +' için ödemeniz gereken ücret ' + str(urunFiyati * urunMiktarı) + ' TL.'
+
+# sonuc =  '{} kg {} için ödemeniz gereken ücret {} TL.' .format(uM, uA, oU)
+
+# f-String
+
+sonuc = f'{uM} kg {uA} için ödemeniz gereken ücret {oU} TL'
+
+print(sonuc)
