40ed9cd01689ee47f2c643741c464c48763917b9
KENAN EVREN KABA <knnevrn@gmail.com>
2025-03-14 02:50:54 +0300

    Listeler ve iç içe listeler ile ilgili denemeler yazıldı.

--- Start.py
+++ Start.py
@@ -2,10 +2,10 @@
 
 # String Format
 
-urunAdi = uA = 'Elma'
-urunFiyati = uF = 5
-urunMiktarı = uM = 2.5
-odenecekUcret = oU = urunFiyati * urunMiktarı 
+# urunAdi = uA = 'Elma'
+# urunFiyati = uF = 5
+# urunMiktarı = uM = 2.5
+# odenecekUcret = oU = urunFiyati * urunMiktarı 
 
 # sonuc = str(urunMiktarı) + ' kg '+ urunAdi +' için ödemeniz gereken ücret ' + str(urunFiyati * urunMiktarı) + ' TL.'
 
@@ -13,6 +13,21 @@ odenecekUcret = oU = urunFiyati * urunMiktarı
 
 # f-String
 
-sonuc = f'{uM} kg {uA} için ödemeniz gereken ücret {oU} TL'
+# sonuc = f'{uM} kg {uA} için ödemeniz gereken ücret {oU} TL'
+
+# Listeler
+
+urunler = [
+    ["Elma ", 5],    # 0. Liste [0. indeks, 1. indeks]
+    ["Armut", 10],   # 1. Liste [0. indeks, 1. indeks]
+    ["Karpuz", 15],  # 2. Liste [0. indeks, 1. indeks]
+    ["Muz", 20],     # 3. Liste [0. indeks, 1. indeks]
+]
+
+# print(urunler[0][0])
+# print(urunler[0][1])
+# print(urunler[3][0])
+
+sonuc = urunler[0][0]
 
 print(sonuc)
